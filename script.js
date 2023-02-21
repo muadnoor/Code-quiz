ffa41db5629922445bc952b5b073853909ffbee5
Yasir Mohamed <muadnoor.27@gmail.com>
2023-02-21 10:21:26 -0600

    set up welcome section

---
+++ script.js
@@ -0,0 +1,6 @@
+const welcome = document.querySelector(".welcome")
+const startBtn = document.querySelector(".startBtn")
+
+
+const quiz = document.querySelector(".quiz")
+const save = document.querySelector(".save")
